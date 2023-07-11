df0dc99caf21c53b0bad76b0c6dedb33f2d88458
technocopia <technocopia@technocopia-ThinkPad-T431s>
2023-07-11 02:20:20 -0400

    Started the frame
    Added the Cylinder

--- FrameForTaser.groovy
+++ FrameForTaser.groovy
@@ -1,1 +1,12 @@
-// code here
+// code here 
+double inch=25.4
+import eu.mihosoft.vrl.v3d.Cylinder
+double cylinderRadius = 3/4*inch
+double cylinderHeight = 5/2*inch 
+double innerRadius = 4/7*inch 
+CSG OutCylinder = new Cylinder(cylinderRadius,cylinderHeight,(int)100).toCSG()
+CSG inCylinder = new Cylinder (innerRadius,cylinderHeight).toCSG()
+CSG Cylinder = OutCylinder.difference(inCylinder);
+
+
+return[Cylinder]
